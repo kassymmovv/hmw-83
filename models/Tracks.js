34b21adcb511dcbf36d9abee8c75fedc3a4543ed
ayan <maxxxlvl1@gmail.com>
2020-03-06 18:47:33 +0600

    added all functions

---
+++ models/Tracks.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const CategorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  album:{
+    type: Schema.Types.ObjectId,
+    ref: 'Album',
+    required: true
+  },
+  duration:{
+    type:String,
+    required:true
+  }
+});
+
+const Track = mongoose.model('Track', CategorySchema);
+
+module.exports = Track;
